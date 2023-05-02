2cb54187cec524c522d17232a7c786d66d094ff6
Amos Macintosh <amos.andreica@stud.ubbcluj.ro>
2023-05-02 09:02:46 +0300

    moved test

--- src/test/java/tasks/ArrayTaskListTest.java
+++ src/test/java/tasks/ArrayTaskListTest.java
@@ -1,10 +1,12 @@
-package tasks.repository;
+package tasks;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import tasks.model.Task;
+import tasks.repository.ArrayTaskList;
+import tasks.repository.TaskList;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
